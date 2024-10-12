5153488d66859895dd3bac7eddd5cea7c14e8553
Prawita Mepilianti <prawitamepilianti@Prawitas-MacBook-Air.local>
2024-10-12 18:44:42 +0700

    cors

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const indexRoutes = require("./src/routes/member.route.js");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const cors = require('cors');
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.use("/v1", indexRoutes);
+app.use(cors());
 
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
